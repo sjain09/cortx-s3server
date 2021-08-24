d35b905f346f245e13909b75bea82ec7f209a70b
Vimal Raghwani <vimal.raghwani@seagate.com>
2021-08-24 07:41:28 -0600

    removed parallel compile
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>

--- third_party/build_motr.sh
+++ third_party/build_motr.sh
@@ -7,5 +7,5 @@ cd motr
 # Uncomment following line to compile motr with both KVS and Cassandra
 # export CONFIGURE_OPTS=--with-cassandra
 PROCESSORS_AVAILABLE=$(cat /proc/cpuinfo | grep '^processor' | wc -l)
-MAKE_OPTS=-j"$PROCESSORS_AVAILABLE" ./scripts/m0 rebuild
+MAKE_OPTS=./scripts/m0 rebuild
 cd ..
